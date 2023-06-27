5030de0c6f25c4bc477791cf4ee9f2540bc29c2b
Valenttinna <fmtinez0@gmail.com>
2023-06-26 21:37:31 -0300

    Seccion definir Destinos

---
+++ Destinos VR/Destinos.py
@@ -0,0 +1,69 @@
+class Viaje:
+    def __init__(self):
+        self.destino = ""
+        self.precio = 0
+
+def espacio():
+    print("\n\n\n\n\n\n\n\n\n\n")
+
+def definirDestino():
+    viaje = Viaje()
+
+    while True:
+        print("-----------------------------------------------------------")
+        print("                 DESTINOS TURISTICOS SAN RAFAEL            ")
+        print("-----------------------------------------------------------")
+        print("                       1 - Valle Grande                    ")
+        print("                       2 - Los Reyunos                     ")
+        print("          3 - El Nihuil y Las Salinas del Diamante         ")
+        print("                       4 - El Sosneado                     ")
+
+        num = int(input())
+
+        if num == 1:
+            viaje.destino = "Valle Grande"
+            viaje.precio = 1800
+            print("El destino seleccionado es:", viaje.destino)
+            print("Precio = $", viaje.precio)
+            espacio()
+            #servicioAdicional()
+            espacio()
+            break
+        elif num == 2:
+            viaje.destino = "Los Reyunos"
+            viaje.precio = 2000
+            print("El destino seleccionado es:", viaje.destino)
+            print("Precio = $", viaje.precio)
+            espacio()
+            #servicioAdicional()
+            espacio()
+            break
+        elif num == 3:
+            viaje.destino = "El Nihuil y Las Salinas del Diamante"
+            viaje.precio = 2500
+            print("El destino seleccionado es:", viaje.destino)
+            print("Precio = $", viaje.precio)
+            espacio()
+            #servicioAdicional()
+            espacio()
+            break
+        elif num == 4:
+            viaje.destino = "El Sosneado"
+            viaje.precio = 3300
+            print("El destino seleccionado es:", viaje.destino)
+            print("Precio = $", viaje.precio)
+            espacio()
+            #servicioAdicional()
+            espacio()
+            break
+        else:
+            print("Los datos ingresados son incorrectos")
+    return viaje
+def main():
+    definirDestino()
+main()
+
+
+
+
+
